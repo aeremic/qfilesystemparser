53293d954cb6f7a034f6d6813a0835a0646339b0
Andrija Eremic <andrija.eremic@gmail.com>
2025-01-12 20:08:58 +0100

    gui

--- FileSystemParser/FileSystemParser.WPF/MainWindow.xaml.cs
+++ FileSystemParser/FileSystemParser.WPF/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace FileSystemParser.WPF
 {
@@ -20,14 +12,47 @@ namespace FileSystemParser.WPF
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private string _path = string.Empty;
+		private string _checkInterval = string.Empty;
+		private string _maximumConcurrentProcessing = string.Empty;
+
 		public MainWindow()
 		{
 			InitializeComponent();
 		}
 
+		private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+		{
+			var textBox = sender as TextBox;
+			e.Handled = Regex.IsMatch(e.Text, "[^0-9]+");
+		}
+
 		private void BrowseButton_Click(object sender, RoutedEventArgs e)
 		{
+			var folderDialog = new OpenFolderDialog();
 
-        }
-    }
+			if (folderDialog.ShowDialog() != true)
+			{
+				return;
+			}
+
+			SelectPathTextBox.Text = folderDialog.FolderName;
+			_path = folderDialog.FolderName;
+		}
+
+		private void CheckIntervalTextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			_checkInterval = CheckIntervalTextBox.Text;
+		}
+
+		private void MaximumConcurrentProcessingTextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			_maximumConcurrentProcessing = MaximumConcurrentProcessingTextBox.Text;
+		}
+
+		private void StartButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
 }
